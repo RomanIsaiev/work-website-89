a0941addd5e6792ef538e6b1195b95908be43a9c
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2025-01-27 15:49:09 +0200

    completed

---
+++ src/js/marquee.js
@@ -0,0 +1,11 @@
+import InfiniteMarquee from 'vanilla-infinite-marquee';
+
+new InfiniteMarquee({
+  element: '.marquee-container',
+  speed: 25000,
+  smoothEdges: false,
+  direction: 'right',
+  gap: '10px',
+  fullContainer: false,
+  spaceBetween: '20px',
+});
